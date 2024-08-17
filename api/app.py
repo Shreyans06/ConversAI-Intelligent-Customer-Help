4614f50206f5f480cc2f9db4fe73c49a5dd8f24a
Shreyans06 <shreyanskjain06@gmail.com>
2024-08-16 22:12:51 -0700

    API-addition but with issues

---
+++ api/app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI , Request
+from langchain.prompts import ChatPromptTemplate
+from langserve import add_routes
+import uvicorn
+import os
+from langchain_community.llms import Ollama
+from dotenv import load_dotenv
+import requests
+from langchain_community.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+app = FastAPI(title= "LangChain Server" , version  = "1.0" , description = "API server")
+
+llm = Ollama(model = "llama2") 
+
+prompt = ChatPromptTemplate.from_template("Write an essay about {topic} with about 100 words.")
+
+add_routes(app,
+           prompt | llm,
+           path="/essay")
+
+if __name__ == "__main__":
+    
+    uvicorn.run(app, host="localhost", port=8000)
+    
+    
+    
---
+++ api/client.py
@@ -0,0 +1,29 @@
+import requests
+import streamlit as st
+
+def get_llm_response(input_text):
+
+    try:
+        # Send a POST request to the LangChain Server
+        response = requests.post("http://localhost:8000/essay/invoke", json={'input': {'topic': input_text}})
+
+        # Check if the request was successful
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured : {e}")
+        return None
+
+    except ValueError:
+        print("Eroor: Unable to decode JSON response")
+        return None
+    
+
+    return response.json()['output']['content']
+
+
+st.title("LangChain Demo with llama2 API")
+input_text = st.text_input("Enter your essay topic")
+
+if input_text:
+    st.write(get_llm_response(input_text))
